163e2e583ffbfe80cda7c8463c8de2e293942e7e
TuanLehuskar <tl28072001@gmail.com>
2022-10-16 23:06:40 +0700

    4 teams finish (HC)

---
+++ Teams/teams.js
@@ -0,0 +1,25 @@
+$('.owl-carousel').owlCarousel({
+    center:true,
+    loop:true,
+    margin:350,
+    nav: true,
+    dots: false,
+    nav: true,
+    navText: ["<span class='fa-solid fa-angle-left'></span>","<span class='fa-solid fa-angle-right'></span>"],
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:3
+        }
+    }
+})
+
+$('.owl-carousel').find('.owl-nav').removeClass('disabled');
+$('.owl-carousel').on('changed.owl.carousel', function(event) {
+	$(this).find('.owl-nav').removeClass('disabled');
+});
