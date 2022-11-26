7d7e2c56ffa5fb199d000d95fc2f30ba04135fdc
Nguyễn Lê Anh Minh <anhminh2122000@gmail.com>
2022-11-27 01:47:12 +0700

    Add Google sheet API + React

--- Blogs/blog.js
+++ Blogs/blog.js
@@ -1,25 +1,59 @@
-$('.owl-carousel').owlCarousel({
-    center:true,
-    loop:true,
-    margin:50,
+async function loadBlogs() {
+  // Get blogs from Spreadsheet
+  const response = await getBlogs(4, 4);
+
+  return new Promise((resolve) => {
+    const blogArea = document.querySelector("#blogArea");
+    ReactDOM.render(
+      response.values.map(([id, title, content, description]) => (
+        <div key={id} className="card-blog card rounded">
+          <img src="../imgs/Blog_1.png" className="card-img-top" />
+          <div className="card-body">
+            <span className="card-time">MAY 30, 2022</span>
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{description}</p>
+            <a href="#" className="read-part d-flex h-auto text-decoration-none">
+              <p className="read-btn-text">Read more</p>
+              <p className="read-btn ms-3 m-0">
+                <i className="fa-solid fa-arrow-right"></i>
+              </p>
+            </a>
+          </div>
+        </div>
+      )),
+      blogArea,
+      resolve
+    );
+  });
+}
+
+const setupOwlCarousel = () => {
+  $(".owl-carousel").owlCarousel({
+    center: false,
+    loop: true,
+    margin: 12,
     nav: true,
     dots: false,
     responsiveClass: true,
-    navText: ["<span class='fa-solid fa-angle-left'></span>","<span class='fa-solid fa-angle-right'></span>"],
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:3
-        },
-        1000:{
-            items:3
-        }
-    }
-})
+    navText: ["<span class='fa-solid fa-angle-left'></span>", "<span class='fa-solid fa-angle-right'></span>"],
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 3,
+      },
+      1000: {
+        items: 4,
+      },
+    },
+  });
+};
 
-$('.owl-carousel').find('.owl-nav').removeClass('disabled');
-$('.owl-carousel').on('changed.owl.carousel', function(event) {
-	$(this).find('.owl-nav').removeClass('disabled');
-});
+async function run() {
+  await loadGapi();
+  await loadBlogs();
+  setupOwlCarousel();
+}
+
+run();
---
+++ sheet-api/initialize.js
@@ -0,0 +1,119 @@
+/* exported gapiLoaded */
+/* exported gisLoaded */
+/* exported handleAuthClick */
+/* exported handleSignoutClick */
+
+// TODO(developer): Set to client ID and API key from the Developer Console
+const CLIENT_ID = "172810885976-hd26i8jfekk71f8d92b7le1qfh4vo9ti.apps.googleusercontent.com";
+const API_KEY = "AIzaSyBBFsidWL6J88hM0zC9uFA3GnyNmAvXSwU";
+
+// Discovery doc URL for APIs used by the quickstart
+const DISCOVERY_DOC = "https://sheets.googleapis.com/$discovery/rest?version=v4";
+
+// Authorization scopes required by the API; multiple scopes can be
+// included, separated by spaces.
+const SCOPES = "https://www.googleapis.com/auth/spreadsheets.readonly";
+
+// document.getElementById('authorize_button').style.visibility = 'hidden';
+// document.getElementById('signout_button').style.visibility = 'hidden';
+
+/**
+ * Initiate google API.
+ */
+async function loadGapi() {
+  loadGsiClient();
+  await loadGapiClient();
+}
+
+/**
+ * Loads the discovery doc to initialize the API.
+ */
+async function loadGapiClient() {
+  return new Promise((resolve, reject) => {
+    async function callback() {
+      await gapi.client.init({
+        apiKey: API_KEY,
+        discoveryDocs: [DISCOVERY_DOC],
+      });
+
+      resolve();
+    }
+
+    gapi.load("client", callback);
+  });
+}
+
+/**
+ * Loads Gsi Client.
+ */
+function loadGsiClient() {
+  window.tokenClient = google.accounts.oauth2.initTokenClient({
+    client_id: CLIENT_ID,
+    scope: SCOPES,
+    callback: "", // defined later
+  });
+}
+
+/**
+ *  Sign in the user upon button click.
+ */
+function handleAuthClick() {
+  if (gapi.client.getToken() === null) {
+    // Prompt the user to select a Google Account and ask for consent to share their data
+    // when establishing a new session.
+    tokenClient.requestAccessToken({ prompt: "consent" });
+  } else {
+    // Skip display of account chooser and consent dialog for an existing session.
+    tokenClient.requestAccessToken({ prompt: "" });
+  }
+}
+
+/**
+ *  Sign out the user upon button click.
+ */
+async function handleSignOutClick() {
+  return new Promise((resolve, reject) => {
+    function callback(response) {
+      if (response.error) {
+        reject(response.error);
+      }
+
+      gapi.client.setToken("");
+      resolve();
+    }
+
+    const token = gapi.client.getToken();
+    if (token !== null) {
+      google.accounts.oauth2.revoke(token.access_token, callback);
+    }
+  });
+}
+
+function onGsiClientLoaded() {
+  console.log("onGsiClientLoaded");
+}
+
+function onGapiLoaded() {
+  console.log("on apis.google.com load");
+}
+
+/**
+ * Spreadsheet of Hugo
+ * https://docs.google.com/spreadsheets/d/1eE6CLFsp90BF_FusTGAtDNO2uPdr3xXZMncHfDh9aSQ/edit
+ */
+const SPREADSHEET_ID = "1eE6CLFsp90BF_FusTGAtDNO2uPdr3xXZMncHfDh9aSQ";
+
+async function getBlogs(skip = 0, take = 4) {
+  // Get data from spreadsheet
+  const response = await gapi.client.sheets.spreadsheets.values.get({
+    spreadsheetId: SPREADSHEET_ID,
+    range: `Blogs!A${skip + 2}:D${skip + take + 2 - 1}`,
+  });
+
+  // Process data
+  const { result } = response;
+  if (!result || !result.values || result.values.length == 0) {
+    return [];
+  }
+  return result;
+}
